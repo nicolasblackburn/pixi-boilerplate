f8c4e3be679d592cb6f70b63b76b64fa2b596a4b
Nicolas Blackburn <nicolas@nblackburn.ca>
2020-05-30 12:53:19 -0400

    Rooms have references of enemies

--- src/map/Room.ts
+++ src/map/Room.ts
@@ -1,9 +1,24 @@
-import { Rectangle } from "pixi-boilerplate/geom";
+import { Rectangle, Point } from "pixi-boilerplate/geom";
+import { TiledMap } from "./TiledMap";
 
 export class Room {
   public bounds: Rectangle;
+  public spawnPoints: Point[];
+  protected enemies: any[];
+  protected map: TiledMap;
 
-  constructor({bounds}: {bounds: Rectangle}) {
+  constructor({bounds, map}: {bounds: Rectangle, map: TiledMap}) {
     this.bounds = bounds;
+    this.enemies = [];
+    this.map = map;
+    this.spawnPoints = [];
+  }
+
+  public addEnemy(enemy) {
+    this.enemies.push(enemy);
+  }
+
+  public getEnemies() {
+    return this.enemies.slice();
   }
 }
